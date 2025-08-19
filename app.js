19cd9814140d2de9e27d8043a599a28b9f03bfc5
Delfina Ferrante <delfinaferrante1@gmail.com>
2025-08-19 18:02:45 -0300

    Funcionalidades: capturar valor del campo de entrada y limpiar el mismo.

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let nombres = []
+
+function agregarAmigo(){
+    let unNombre = document.getElementById('amigo').value;
+    alert(unNombre);
+    limpiarInput()
+}
+
+function limpiarInput(){
+    let valor = document.querySelector('#amigo');
+    valor.value = '';
+}
